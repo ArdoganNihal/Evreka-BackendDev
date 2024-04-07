fd22458cd2b49611a32453e6cf113bb6e52f3efb
Nihal Ardoğan <nihalardogan@Nihal-MacBook-Pro.local>
2024-04-08 01:38:50 +0300

    delete devices, list devices, get device

--- app/schemas.py
+++ app/schemas.py
@@ -1,15 +1,19 @@
 from pydantic import BaseModel
 
 class DeviceDataCreate(BaseModel):
-    id:int
     route: str
     service_point: str
     mrf: str
     parcel: str
     gps: str
 
-class DeviceData(DeviceDataCreate):
+class DeviceData(BaseModel):
     id: int
+    route: str
+    service_point: str
+    mrf: str
+    parcel: str
+    gps: str
 
     class Config:
         orm_mode = True
